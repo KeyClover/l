4aad9ea5f2d0aad8660b308b7620bf170ca90bd4
KeyClover <suvichak0001@gmail.com>
2022-04-14 20:30:50 +0700

    LCD clearScreen using for loop (version 5). Update: replace all the message with the printMessage

--- src/com/company/LCD.java
+++ src/com/company/LCD.java
@@ -3,6 +3,9 @@ package com.company;
 public class LCD {
     public static int HEIGHT = 25; //default HEIGHT for the LCD display
     public static int WIDTH = 80;  //default WIDE for the LCD display
+    public static int CENTER = 1; // alignment to the center
+    public static int RIGHT =2;   // alignment to the right
+    public static int LEFT = 3;  //alignment to the left
 
     public static void clearScreen() {
 
@@ -10,36 +13,55 @@ public class LCD {
             System.out.println();
         }
 
-
-
     }
+
+    // ALT+218 = ┌  //ALT+191 = ┐ // ALT+179 = │ // ALT+196 = ─ //ALT+192 = └ //ALT+217 = ┘
+    //ALT+195 =├ //ALT+180= ┤
 
     public static void showMainMenu() {
         clearScreen();
         {
-            // ALT+218 = ┌  //ALT+191 = ┐ // ALT+179 = │ // ALT+196 = ─ //ALT+192 = └ //ALT+217 = ┘
+            topConner();
 
-            System.out.print("┌"); // top right conner
-            dashLine(); //Dash between Top right conner and Left conner
-            System.out.println("┐"); // top left conner
+            printMessage("Welcome to Stamford LCD Library Demo", CENTER);
+            blankLine();
+            printMessage("Select a Function (1-3)", 1); // alignment is CENTER
 
-            System.out.println("|    Welcome to Stamford LCD Library Demo  |");
-            System.out.println("|                                          |");
-            System.out.println("|        Select a Function (1-3)           |");
+            middleLine();
 
-            System.out.print("│"); //Middle left
-            dashLine();// A dash line between Middle Left and Middle right line
-            System.out.println("│");//Middle right
-
-            System.out.println("|                                          |");
-            System.out.println("|                  1. Login                |");
-            System.out.println("|                  2. Restart              |");
-            System.out.println("|                  3. Shutdown             |");
-
-            System.out.print("└"); // Bottom left conner
-            dashLine(); //dash line between bottom left conner and right conner
-            System.out.println("┘"); // Bottom right conner
+            blankLine();
+            printMessage("1. Login", 3);
+            printMessage("2. Restart", 3);
+            printMessage("3. Shutdown",3);
+            bottomConner();
         }
+
+    }
+    public static void printMessage(String message, int alignment) {
+        System.out.print("│ ");
+        System.out.print("message length =" + message.length());
+        System.out.println(message);
+
+    }
+
+    public static void topConner() {
+        System.out.print("┌"); // top right conner
+        dashLine(); //Dash between Top right conner and Left conner
+        System.out.println("┐"); // top left conner
+    }
+
+    public static void middleLine() {
+        System.out.print("├"); //Middle left
+        dashLine();// A dash line between Middle Left and Middle right line
+        System.out.println("┤");//Middle right
+
+
+    }
+
+    public static void bottomConner() {
+        System.out.print("└"); // Bottom left conner
+        dashLine(); //dash line between bottom left conner and right conner
+        System.out.println("┘"); // Bottom right conner
 
     }
 
@@ -49,84 +71,90 @@ public class LCD {
         }
     }
 
+    public static void blankLine() {
+        System.out.print("│");
+        for(int i = 0; i<= WIDTH-2; i++) {
+            System.out.print(" ");
+        }
+        System.out.println("│");
+    }
 
 
 
     public static void showLoginMenu() {
-        System.out.println(" -------------------------------------------- ");
-        System.out.println("| Login Menu. Press F1 for more information. |");
-        System.out.println("|--------------------------------------------|");
-        System.out.println("|                                            |");
-        System.out.println("| Enter Username:                            |");
-        System.out.println(" -------------------------------------------- ");
+        topConner();
+        printMessage("Login Menu. Press F1 for more information", 3);
+        blankLine();
+        printMessage("Enter Username:",3);
+        bottomConner();
     }
 
     public static void showSystemMenu() {
 
-        System.out.println(" ------------------------------------------ ");
-        System.out.println("| Select a Function (1-4).                 |");
-        System.out.println("|------------------------------------------|");
-        System.out.println("|                                          |");
-        System.out.println("| 1. Change AC Temperature set point       |");
-        System.out.println("| 2. Enable/Disable IP Camera              |");
-        System.out.println("| 3. Turn anti-theft system on/off.        |");
-        System.out.println("| 4. Exit                                  |");
-        System.out.println(" ------------------------------------------ ");
+        topConner();
+        printMessage(" Select a Function (1-4).",3);
+        middleLine();
+        blankLine();
+        printMessage("1. Change AC Temperature set point",3);
+        printMessage("2. Enable/Disable IP Camera",3);
+        printMessage("3. Turn anti-theft system on/off.",3);
+        printMessage("4. Exit",3);
+        bottomConner();
     }
 
     public static void changeACSetP() {
 
-        System.out.println(" ------------------------------------------ ");
-        System.out.println("| Applying New Temperature Set Point.      |");
-        System.out.println("|------------------------------------------|");
-        System.out.println("|                                          |");
-        System.out.println("| New temperature set point will be in     |");
-        System.out.println("| effect soon...                           |");
-        System.out.println(" ------------------------------------------ ");
+        topConner();
+        printMessage("Applying New Temperature Set Point.",3);
+        middleLine();
+        blankLine();
+        printMessage("New temperature set point will be in",3);
+        printMessage("effect soon...",3);
+        bottomConner();
         showRestart();
 
     }
 
     public static void confirmRestart() {
-        System.out.println(" ------------------------------------------ ");
-        System.out.println("| Restart Confirmation                     |");
-        System.out.println("|------------------------------------------|");
-        System.out.println("|                                          |");
-        System.out.println("| Are you sure that you want to restart    |");
-        System.out.println("| the system? (Y/N)                        |");
-        System.out.println(" ------------------------------------------ ");
+        topConner();
+        printMessage("Restart Confirmation",3);
+        middleLine();
+        blankLine();
+        printMessage("Are you sure that you want to restart",3);
+        printMessage("the system? (Y/N)", 3);
+        bottomConner();
 
     }
 
     public static void confirmShutdown() {
 
-        System.out.println(" ------------------------------------------ ");
-        System.out.println("| Shutdown Confirmation                    |");
-        System.out.println("|------------------------------------------|");
-        System.out.println("|                                          |");
-        System.out.println("| Are you sure that you want to shutdown   |");
-        System.out.println("| the system? (Y/N)                        |");
-        System.out.println(" ------------------------------------------ ");
+        topConner();
+        printMessage("Shutdown Confirmation", 3);
+        middleLine();
+        blankLine();
+        printMessage("Are you sure that you want to shutdown",3);
+        printMessage("the system? (Y/N)",3);
+        bottomConner();
     }
     public static void showRestart() {
 
-        System.out.println(" ------------------------------------------ ");
-        System.out.println("| Restarting ....                          |");
-        System.out.println("|------------------------------------------|");
-        System.out.println("|                                          |");
-        System.out.println("| Please wait while the system is          |");
-        System.out.println("| preparing for a restart.                 |");
-        System.out.println(" ------------------------------------------ ");
+        topConner();
+        printMessage("Restarting ....",3);
+        middleLine();
+        blankLine();
+        printMessage("Please wait while the system is",3);
+        printMessage("preparing for a restart.",3);
+        bottomConner();
     }
     public static void showShutdown() {
 
-        System.out.println(" ------------------------------------------ ");
-        System.out.println("| Shutting down ....                       |");
-        System.out.println("|------------------------------------------|");
-        System.out.println("|                                          |");
-        System.out.println("| Please wait while the system is          |");
-        System.out.println("| preparing for a shutdown.                |");
-        System.out.println(" ------------------------------------------ ");
+        topConner();
+        printMessage("Shutting down ....",3);
+        middleLine();
+        blankLine();
+        printMessage("Please wait while the system is",3);
+        printMessage("preparing for a shutdown.",3);
+        bottomConner();
 }
 
 }
